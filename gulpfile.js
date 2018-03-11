d2b2cf9abb41363518a7788b62e1ecc487aa1736
Steve <whooody@gmail.com>
2018-03-11 18:53:55 -0500

    split css into separate files

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+cssnested = require('postcss-nested'),
+cssimport = require('postcss-import');
+
+gulp.task('html', function() {
+
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssimport, cssvars, cssnested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+
+});
+
+gulp.task('watch', function() {
+
+	watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	});
+
+});

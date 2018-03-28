6fe6ea09a70d2fca6c0506d41433a7f90d8bee2e
Steve <whooody@gmail.com>
2018-03-27 22:39:12 -0500

    finished styling header and added javascript code for mobile menu

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 cssnested = require('postcss-nested'),
 cssimport = require('postcss-import')
-mixins= require('postcss-mixins');
+mixins= require('postcss-mixins'),
+hexrgba= require('postcss-hexrgba');
 
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssimport, mixins, cssvars, cssnested, autoprefixer]))
+	.pipe(postcss([cssimport, mixins, cssvars, cssnested, hexrgba, autoprefixer]))
 	.on('error', function(errorInfo) {
 		console.log(errorInfo.toString());
 		this.emit('end');
